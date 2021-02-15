6e83f7935f5ce4c261920faa86abbdcfba5e9af8
Takuya Hirata <takuyahirata4@gmail.com>
2021-02-15 18:08:46 -0500

    feat: Field except password

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Layout } from '../src/components'
+import { Field, Form } from '../src/elements'
 
 const SignUp: React.FC = () => {
-  return <Layout minHeight>sign up</Layout>
+  const [inputs, setInputs] = React.useState('here')
+  return (
+    <Layout minHeight>
+      <Form>
+        <Field
+          name="username"
+          label="Username"
+          onChange={e => setInputs(e.target.value)}
+          value={inputs}
+        />
+      </Form>
+    </Layout>
+  )
 }
 
 export default SignUp
--- src/elements/Field/Field.tsx
+++ src/elements/Field/Field.tsx
@@ -1,10 +1,67 @@
-import React from 'react'
-import { TextField } from '@material-ui/core'
+import React, { createElement } from 'react'
+import { Grid, TextField } from '@material-ui/core'
+import {
+  AccountBox,
+  Email,
+  Visibility,
+  VisibilityOff,
+} from '@material-ui/icons'
 
-interface Props {}
+const icons = {
+  username: AccountBox,
+  email: Email,
+  password: VisibilityOff,
+  text: Visibility,
+}
 
-const Field: React.FC<Props> = () => {
-  return null
+type GridItemProps = {
+  xs?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'auto' | 10 | 11 | 12 | boolean
+  sm?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'auto' | 10 | 11 | 12 | boolean
+  md?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'auto' | 10 | 11 | 12 | boolean
+  lg?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'auto' | 10 | 11 | 12 | boolean
+  xl?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'auto' | 10 | 11 | 12 | boolean
+}
+
+interface Props {
+  GridItemProps?: GridItemProps
+  label?: string
+  name: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  type?: string
+  value: string
+}
+
+const Field: React.FC<Props> = ({
+  GridItemProps = {
+    xs: 12,
+    sm: 12,
+    md: 12,
+    lg: 12,
+    xl: 12,
+  },
+  label,
+  name,
+  onChange,
+  type = 'text',
+  value,
+}) => {
+  return (
+    <Grid item {...GridItemProps}>
+      <TextField
+        label={label || name}
+        name={name}
+        onChange={onChange}
+        value={value}
+        InputProps={{
+          //@ts-ignore
+          endAdornment: createElement(icons[name]),
+        }}
+        type={type}
+        variant="outlined"
+        fullWidth
+      />
+    </Grid>
+  )
 }
 
 export default Field
