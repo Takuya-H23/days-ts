4995c88ce861a56f638e39c27db59f174dfe1d4a
Takuya Hirata <takuyahirata4@gmail.com>
2021-02-14 22:44:32 -0500

    chore: material ui setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import {AppProps } from 'next/app'
+import Head from 'next/head'
+import React from 'react'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Days</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>)
 }
 
