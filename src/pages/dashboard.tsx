391985121cedd31696f727e76bb6a9f3cacd91c7
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-01 08:29:52 -0500

    feat: basic structure

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,23 @@
+import Link from 'next/link'
 import { Typography } from '@material-ui/core'
 import { DashboardLayout } from '../components'
+import { Dashboard as DashboardIcon } from '@material-ui/icons'
 
 export default function Dashboard() {
   return (
     <DashboardLayout
       left={
         <Typography variant="body1" color="textPrimary">
-          Dashboard
+          <ul>
+            <li>
+              <Link href="/signUp">
+                <a>
+                  <DashboardIcon />
+                  Dashboard
+                </a>
+              </Link>
+            </li>
+          </ul>
         </Typography>
       }
       right={
