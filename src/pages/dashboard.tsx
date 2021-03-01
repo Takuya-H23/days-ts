6fa77919535c6463e525a7527f344aedfcab62a4
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-01 07:35:43 -0500

    fix: merge conflicts in neovim

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,26 +1,19 @@
-import { Grid, Box } from '@material-ui/core'
-import { Header } from '../components'
+import { Typography } from '@material-ui/core'
+import { DashboardLayout } from '../components'
 
-interface Props {
-  children: React.ReactNode
-}
-
-export default function DashboardLayout({ children }: Props) {
+export default function Dashboard() {
   return (
-    <>
-      <Header />
-      <Grid container>
-        <Grid item xs={5} sm={4} md={3} lg={2} xl={2}>
-          <Box p={[2, 2, 2, 2, 2]}>
-            <div>navs</div>
-          </Box>
-        </Grid>
-        <Grid item xs={7} sm={8} md={9} lg={10} xl={10}>
-          <Box p={[2, 2, 2, 2, 2]} style={{ border: '1px solid' }}>
-            {children}
-          </Box>
-        </Grid>
-      </Grid>
-    </>
+    <DashboardLayout
+      left={
+        <Typography variant="body1" color="textPrimary">
+          Dashboard
+        </Typography>
+      }
+      right={
+        <Typography variant="body1" color="textPrimary">
+          content
+        </Typography>
+      }
+    />
   )
 }
