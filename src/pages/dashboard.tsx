13ce1155fb6e8c03539ba4bc19328e4940d89e5b
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-01 20:52:55 -0500

    feat: useHello test

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,20 @@
-import { Typography } from '@material-ui/core'
+import {} from '@material-ui/core'
+import { useQuery } from 'react-query'
+import { request, gql } from 'graphql-request'
 import { DashboardLayout } from '../components'
+import { END_POINT } from '../utils/constants/routes'
+
+const query = gql`
+  query {
+    hello
+  }
+`
+
+const useHello = () =>
+  useQuery(['hello'], async () => await request(END_POINT, query))
 
 export default function Dashboard() {
-  return (
-    <DashboardLayout>
-      <Typography variant="body1" color="textPrimary">
-        content
-      </Typography>
-    </DashboardLayout>
-  )
+  const data = useHello()
+  console.log(data)
+  return <DashboardLayout>content will be here</DashboardLayout>
 }
