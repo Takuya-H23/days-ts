1deea80ebb30629e0c001c4ee4ca02c4abb8053c
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-02 07:48:14 -0500

    feat: note category graphql typeDef

--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -1,13 +1,14 @@
 import { gql } from 'apollo-server-micro'
 
 export default gql`
-  type Query  {
+  type Query {
     hello: String!
   }
 
   type Mutation {
     signIn(input: SignInInput): User!
     signUp(input: SignUpInput): User!
+    createNoteCategory(input: CreateNoteCategory): Category!
   }
 
   type User {
@@ -16,6 +17,13 @@ export default gql`
     email: String!
     created_at: String!
     last_login: String
+  }
+
+  type Category {
+    category_id: ID!
+    category: String!
+    created_at: Stirng!
+    updated_at: Stirng
   }
 
   input SignInInput {
@@ -27,5 +35,9 @@ export default gql`
     username: String!
     email: String!
     password: String!
+  }
+
+  input CreateNoteCategory {
+    category: String!
   }
 `
