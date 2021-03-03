c994eed0ac36bf4e9504ec4b6e54ec3f12949e32
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-03 06:21:15 -0500

    feat: note catogory resolver setup

--- src/graphql/resolvers/createNoteCategory.ts
+++ src/graphql/resolvers/createNoteCategory.ts
@@ -2,6 +2,10 @@ import { pipe } from 'fp-ts'
 import * as TE from 'fp-ts/TaskEither'
 import * as E from 'fp-ts/Either'
 
-export default async function createNoteCatogory() {
+export default async function createNoteCatogory(
+  _,
+  { input },
+  { pool, cookies }
+) {
   return { note_category_id: 1, category: 'test', created_at: 'test' }
 }
