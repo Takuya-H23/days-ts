7623aec9a2c74dbd33711bad44df03d87db5572b
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-11 08:54:02 -0500

    WIP: reducer

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useQuery } from 'react-query'
+import { lensProp, over } from 'ramda'
 import { request, gql } from 'graphql-request'
 import { END_POINT } from '../utils/constants/routes'
 
@@ -27,17 +27,38 @@ const query = gql`
   }
 `
 
-const useFetchUser = () => {
-  return useQuery('user', async () => request(END_POINT, query), {
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-  })
+// const useFetchUser = () => {
+//   return useQuery('user', async () => request(END_POINT, query), {
+//     refetchOnMount: false,
+//     refetchOnWindowFocus: false,
+//     refetchOnReconnect: false,
+//   })
+// }
+
+const fetchUser = () => request(END_POINT, query)
+
+const L = {
+  user: lensProp('user'),
+  isLoading: lensProp('isLoading'),
+}
+
+const actions = {
+  fetchUser,
+}
+
+const reducer = (state, { type, payload }) => {
+  return actions[type](state, payload)
 }
 
 export default function AuthProvider({ children }: Props) {
-  const { isLoading, data, isError } = useFetchUser()
-  const value = {}
+  const [state, dispatch] = React.useReducer(reducer, iv)
 
+  React.useEffect(() => {
+    fetchUser().then(d => dispatch({ type: 'fetchUser', payload: d.fetchUser }))
+    //    fetchUser().then(console.log)
+  }, [])
+  console.log('state', state)
+
+  const value = {}
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
