02b1ac97df2e31562caf883c6099e6e59090b9d2
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-12 08:47:26 -0500

    WIP: auth reducer

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { lensProp, over } from 'ramda'
+import { compose, lensProp, over } from 'ramda'
 import { request, gql } from 'graphql-request'
 import { END_POINT } from '../utils/constants/routes'
 
@@ -7,8 +7,21 @@ interface InitialValues {
   [key: string]: any
 }
 
+type User = {
+  [kye: string]: any
+}
+
+type State = {
+  isLoggedIn: boolean
+  isLoading: boolean
+  user: User | null
+}
+
+type Action = { type: string; payload?: any }
+
 const iv: InitialValues = {
   isLoggedIn: false,
+  isLoading: false,
   user: null,
 }
 
@@ -27,28 +40,29 @@ const query = gql`
   }
 `
 
-// const useFetchUser = () => {
-//   return useQuery('user', async () => request(END_POINT, query), {
-//     refetchOnMount: false,
-//     refetchOnWindowFocus: false,
-//     refetchOnReconnect: false,
-//   })
-// }
-
 const fetchUser = () => request(END_POINT, query)
 
 const L = {
   user: lensProp('user'),
   isLoading: lensProp('isLoading'),
+  isLoggedIn: lensProp('isLoggedIn'),
 }
 
-const init = (...x) => console.log(x)
+const init = (state: State) => over(L.isLoading, () => true, state)
 
-const actions = {
+const updateUser = (state: State, payload: User) =>
+  compose(
+    over(L.isLoading, () => false),
+    over(L.isLoggedIn, () => true),
+    over(L.user, () => payload)
+  )(state)
+
+const actions: Record<string, any> = {
   init,
+  updateUser,
 }
 
-const reducer = (state, { type, payload }) => {
+const reducer = (state: any, { type, payload }: any) => {
   return actions[type](state, payload)
 }
 
@@ -56,11 +70,14 @@ export default function AuthProvider({ children }: Props) {
   const [state, dispatch] = React.useReducer(reducer, iv)
 
   React.useEffect(() => {
+    dispatch({ type: 'init' })
+
     fetchUser()
-      .then(d => dispatch({ type: 'init', payload: d.fetchUser }))
+      .then(d => dispatch({ type: 'updateUser', payload: d.fetchUser }))
       .catch(console.error)
   }, [])
-  console.log('state', state)
+
+  console.log(state)
 
   const value = {}
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
